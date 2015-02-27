d86aaf26d609bd4e069290c9c7a6a2f308a5a773
elvios <elvis.flesborg@gmail.com>
2015-02-27 12:55:32 +0100

    Buginfo finds line numbers and column numbers

--- categorize_errors.py
+++ categorize_errors.py
@@ -7,7 +7,7 @@ logdir = 'results/' + logmd5
 lines = [line.strip() for line in open(logdir + '/buginfo')]
 
 bugs = []
-file_names = []
+files = []
 
 for line in lines:
 
@@ -16,7 +16,7 @@ for line in lines:
 
     #if not re.search(r"\^", line) == None:
     if line.strip() == "^":
-        bugs.append([bugtype, file_names])
+        bugs.append([bugtype, files])
         del bugtype
         files = []
         continue
@@ -25,10 +25,18 @@ for line in lines:
     if not bugtype_re == None:
         bugtype = bugtype_re.group(0)
 
-    filename_re = re.search(r"(\w+\/)*\w+(\.c|\.h|\.o)", line)
+    filename_re = re.search(r"(\w+\/)*\w+(\.c|\.h|\.o)", line) # `.o` or not?
     if not filename_re == None:
-        file_names.append(filename_re.group(0))
+        filename = filename_re.group(0)
+    
+        lines_cols_re = re.search(r":[0-9]*:[0-9]*(:|,)", line)
+        lines_re = re.search(r":[0-9].*(:|,)", line)
+        if not lines_cols_re == None:
+            lines_cols = lines_cols_re.group(0)
+        elif not lines_re == None:
+            lines_cols = lines_re.group(0)
 
+        files.append([filename, lines_cols])
     
 
 for bug in bugs:
