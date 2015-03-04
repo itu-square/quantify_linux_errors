9c796b946d58dccbd3562e5414d079438169e5c1
elvios <elvis.flesborg@gmail.com>
2015-03-04 13:00:05 +0100

    Documentation added, and more features for check_randconfigs.sh

--- check_randconfigs.sh
+++ check_randconfigs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -en "Total\tnot set\t=n\t=y\tmod\tsum\tvar\n"
+echo -en "Total\tunset\t=n\t=y\tmod\tsum\tvar\n"
 function find_ratio
 {
     var="$1"
@@ -22,9 +22,27 @@ function find_ratio
 
 
 find_ratio "CONFIG_64BIT"
-find_ratio "CONFIG_GENERIC_CLOCKEVENTS"
-find_ratio "CONFIG_GENERIC_IRQ_PROBE"
-find_ratio "CONFIG_INSTRUCTION_DECODER"
-find_ratio "CONFIG_GENERIC_HWEIGHT"
-find_ratio "CONFIG_FONT_8x16"
 find_ratio "CONFIG_SERIAL_TIMBERDALE"
+find_ratio "CONFIG_GENERIC_HWEIGHT"
+find_ratio "CONFIG_ARCH_HWEIGHT_CFLAGS"
+find_ratio "CONFIG_GENERIC_IRQ_LEGACY_ALLOC_HWIRQ"
+find_ratio "CONFIG_HAVE_HW_BREAKPOINT"
+find_ratio "CONFIG_NET_DSA_HWMON"
+find_ratio "CONFIG_MAC80211_MHWMP_DEBUG"
+find_ratio "CONFIG_TOUCHSCREEN_TOUCHWIN"
+find_ratio "CONFIG_HW_CONSOLE"
+find_ratio "CONFIG_VT_HW_CONSOLE_BINDING"
+find_ratio "CONFIG_HW_RANDOM"
+find_ratio "CONFIG_HW_RANDOM_TIMERIOMEM"
+find_ratio "CONFIG_HW_RANDOM_INTEL"
+find_ratio "CONFIG_HW_RANDOM_AMD"
+find_ratio "CONFIG_HW_RANDOM_GEODE"
+find_ratio "CONFIG_HW_RANDOM_VIA"
+find_ratio "CONFIG_HW_RANDOM_VIRTIO"
+find_ratio "CONFIG_HWMON"
+find_ratio "CONFIG_HWMON_VID"
+find_ratio "CONFIG_HWMON_DEBUG_CHIP"
+find_ratio "CONFIG_SENSORS_IIO_HWMON"
+find_ratio "CONFIG_USB_HWA_HCD"
+find_ratio "CONFIG_UWB_HWA"
+find_ratio "CONFIG_CRYPTO_HW"
