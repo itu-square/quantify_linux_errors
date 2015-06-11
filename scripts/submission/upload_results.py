568ff3586292a3acf0528872de2427d6d1721025
elvios <elvis.flesborg@gmail.com>
2015-06-11 11:19:56 +0200

    Submitting the config file now works

--- scripts/submission/upload_results.py
+++ scripts/submission/upload_results.py
@@ -2,6 +2,7 @@ import sys, os
 import json
 import pymysql
 import base64
+import re
 
 
 # Error catching and usage
@@ -20,7 +21,8 @@ cat_file = "categorized"
 db = pymysql.connect(host='mysql.itu.dk',
     user='elvis_thesis',
     passwd='linux',
-    db='elvis_thesis')
+    db='elvis_thesis',
+    charset='utf8')
 
 cursor = db.cursor()
 
@@ -30,12 +32,15 @@ cursor = db.cursor()
 for _, dirs, _ in os.walk(results_dir):
     for dir in dirs:
         if not dir in ['gcc', 'archive']:
-            config = open(results_dir + dir + "/config", 'r').read().encode('ascii')
-            config_b64 = base64.b64encode(config)
-            cursor.execute("insert into configurations (hash, original) values ('" + 
-                dir + "', '" + 
-                config_b64 + "')")
-
+            config = open(results_dir + dir + "/config", 'r').read()
+            config = re.escape(config)
+            #config_b64 = base64.b64encode(config)
+            
+            # Inserting configuration into database
+            #values = dir + ", " + re.escape(str(config))
+            query = "insert into configurations (hash, original) values (\"%s\", \"%s\");"
+            print(query % (dir, dir))
+            cursor.execute(query % (dir, config))
 
 
 # Getting the categorized warnings
