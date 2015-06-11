a18557fb8ef8c4be31865713c1a6674d756c5539
elvios <elvis.flesborg@gmail.com>
2015-06-11 17:55:48 +0200

    Added a simple run.sh script to do all the work

---
+++ run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$1" == "" ]
+then
+    echo "Missing argument"
+    exit
+fi
+
+if [ "$2" == "" ]
+then
+    runs=1
+else
+    runs="$2"
+fi 
+
+python scripts/compilation/make_mrproper.py "$1"
+
+for i in `seq 1 "$runs"`
+do
+    python scripts/compilation/make_config.py "$1"
+    python scripts/compilation/make.py "$1"
+    python scripts/categorization/categorize_errors.py "$1"
+    python scripts/submission/upload_results.py "$1"
+    python scripts/submission/backup_results.py "$1"
+done
